193eaaf6aea003878bf4204a69dc85fb9e3e7b05
oscar <ovinan@gmail.com>
2023-01-14 17:10:06 +0100

    Actualizados comentarios conexion consola H2

--- src/main/java/com/example/demo/CrudH2Application.java
+++ src/main/java/com/example/demo/CrudH2Application.java
@@ -9,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class CrudH2Application {
-
+        // Si en el fichero application.properties tenemos habilitado: spring.h2.console.enabled=true
+        // ..podremos conectarnos al editor de la base de datos H2 en la URL: http://localhost:8080/h2-console.
+        // NOTA: El fichero application.properties esta en src/main/resources/default-package
 	public static void main(String[] args) {
 		SpringApplication.run(CrudH2Application.class, args);
 	}
